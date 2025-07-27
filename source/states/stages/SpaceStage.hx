2749c49cf251813ab8f0e703899b38f2a0d0f9d1
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-07-27 08:50:38 -0400

    Update SpaceStage.hx

--- source/states/stages/SpaceStage.hx
+++ source/states/stages/SpaceStage.hx
@@ -7,7 +7,7 @@ class SpaceStage extends BaseStage
 	override function create()
 	{
 				var bg:FlxSprite = new FlxSprite(-600, -200).loadGraphic(Paths.image('space'));
-				bg.antialiasing = ClientPrefs.globalAntialiasing;
+				bg.antialiasing = ClientPrefs.data.antialiasing;
 				bg.scrollFactor.set(0.9, 0.9);
 				bg.active = false;
 				add(bg);
@@ -15,7 +15,7 @@ class SpaceStage extends BaseStage
 				var asteroid:FlxSprite = new FlxSprite(-650, 600).loadGraphic(Paths.image('asteroid'));
 				asteroid.setGraphicSize(Std.int(asteroid.width * 1.1));
 				asteroid.updateHitbox();
-				asteroid.antialiasing = ClientPrefs.globalAntialiasing;
+				asteroid.antialiasing = ClientPrefs.data.antialiasing;
 				asteroid.scrollFactor.set(0.9, 0.9);
 				asteroid.active = false;
 				add(asteroid);
@@ -24,7 +24,7 @@ class SpaceStage extends BaseStage
 					var voyager:FlxSprite = new FlxSprite(-500, -300).loadGraphic(Paths.image('voyager'));
 					voyager.setGraphicSize(Std.int(voyager.width * 0.9));
 					voyager.updateHitbox();
-					voyager.antialiasing = ClientPrefs.globalAntialiasing;
+					voyager.antialiasing = ClientPrefs.data.antialiasing;
 					voyager.scrollFactor.set(1.3, 1.3);
 					voyager.active = false;
 					add(voyager);
