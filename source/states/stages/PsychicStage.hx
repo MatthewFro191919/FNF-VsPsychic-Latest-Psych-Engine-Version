29fbafee528e0ba9a730e7c188c016c98f01f6ed
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-07-27 16:14:40 -0400

    Update PsychicStage.hx

--- source/states/stages/PsychicStage.hx
+++ source/states/stages/PsychicStage.hx
@@ -3,6 +3,7 @@ package states.stages;
 import states.stages.objects.*;
 import cutscenes.DialogueBox;
 import shaders.WiggleEffect.WiggleEffectType;
+import objects.Note;
 
 class PsychicStage extends BaseStage
 {
@@ -32,4 +33,333 @@ class PsychicStage extends BaseStage
 	{
 				if(!ClientPrefs.data.lowQuality) animatedBGSprite.dance();
 	}
+
+	function finishSong():Void
+	{
+		var finishCallback:Void->Void = endSong;
+		if(isStoryMode) {
+			switch(SONG.song.toLowerCase()) {
+				case 'psychic': {
+					finishCallback = psychicEndSong;
+				}
+			}
+		}
+
+		updateTime = false;
+		FlxG.sound.music.volume = 0;
+		vocals.volume = 0;
+		vocals.pause();
+		if(ClientPrefs.data.noteOffset <= 0) {
+			finishCallback();
+		} else {
+			finishTimer = new FlxTimer().start(ClientPrefs.data.noteOffset / 1000, function(tmr:FlxTimer) {
+				finishCallback();
+			});
+		}
+	}
+
+	function achievementEnd():Void
+	{
+		achievementObj = null;
+		if(endingSong && !inCutscene) {
+			endSong();
+		}
+	}
+
+	var camFrontEffect:FlxCamera;
+	function psychicEndSong():Void //This function will be removed once we add a second song to Psychic's week, as the cutscene will happen at the start of the second song instead of end of the first one
+	{
+		songPercent = 1.0;
+		timeTxt.text = '0:00';
+		inCutscene = true;
+		endingSong = true;
+		canPause = false;
+		camZooming = false;
+
+		deathCounter = 0;
+		seenCutscene = false;
+		updateTime = false;
+		KillNotes();
+
+		// Cutscene
+		FlxTween.tween(camHUD, {alpha: 0}, 2);
+
+		FlxTween.tween(psychicBlack, {alpha: 0.8}, 6, {startDelay: 1.5});
+		FlxTween.tween(FlxG.camera, {zoom: 1}, 8, {ease: FlxEase.sineInOut});
+
+		FlxG.camera.follow(camFollowPos, LOCKON, 1);
+		camFollow.set(camFollowPos.x, camFollowPos.y);
+		FlxTween.tween(camFollowPos, {x: boyfriend.getGraphicMidpoint().x, y: boyfriend.getGraphicMidpoint().y - 50}, 6, {ease: FlxEase.circOut});
+		boyfriend.specialAnim = true;
+		gf.stunned = true;
+		dad.stunned = true;
+
+		camFrontEffect.follow(camFollowPos, LOCKON, 1);
+		frontFakeBf.cameras = [camFrontEffect];
+		FlxTween.tween(frontFakeBf.scale, {x: 1.2, y: 1.2}, 3.75, {startDelay: 7.5, ease: FlxEase.circOut});
+		FlxTween.tween(frontFakeBf, {alpha: 0}, 3.75, {startDelay: 7.5});
+
+		wiggleShit.effectType = FLAG;
+		camFrontEffect.setFilters([new ShaderFilter(wiggleShit.shader)]);
+
+		boyfriend.visible = false;
+		frontFakeBf.visible = true;
+		frontFakeBf.animation.play('idle', true);
+		fakeBf.visible = true;
+		fakeBf.animation.play('idle', true);
+		fakeBf.antialiasing = ClientPrefs.globalAntialiasing;
+
+		new FlxTimer().start(7.5, function(tmr:FlxTimer) {
+			wiggleShit.setValue(0);
+			wiggleShit.waveSpeed = 5;
+			FlxTween.tween(wiggleShit, {waveFrequency: 0.03, waveAmplitude: 0.03}, 2.5);
+			FlxG.camera.shake(0.012, 1, function() {
+				FlxG.camera.shake(0.009, 1, function() {
+					FlxG.camera.shake(0.006, 1, function() {
+						FlxG.camera.shake(0.003, 1);
+					});
+				});
+			});
+			camFrontEffect.shake(0.012, 1, function() {
+				camFrontEffect.shake(0.009, 1, function() {
+					camFrontEffect.shake(0.006, 1, function() {
+						camFrontEffect.shake(0.003, 1);
+					});
+				});
+			});
+		});
+
+		new FlxTimer().start(11.5, function(tmr:FlxTimer) {
+			var black:FlxSprite = new FlxSprite(-200,-200).makeGraphic(FlxG.width * 2, FlxG.height * 2, FlxColor.BLACK);
+			black.scrollFactor.set();
+			black.alpha = 0;
+			add(black);
+			FlxTween.tween(black, {alpha: 1}, 3, {onComplete: function(twn:FlxTween) {
+				fakeBf.visible = false;
+				new FlxTimer().start(1, function(tmr:FlxTimer) {
+					var toBeContinued:Alphabet = new Alphabet(0, 0, "To be continued", true, true, 0.1);
+					toBeContinued.scrollFactor.set();
+					toBeContinued.screenCenter();
+					toBeContinued.x -= 425; //No funny weed number for you
+					toBeContinued.y -= 105;
+					add(toBeContinued);
+
+					var black:FlxSprite = new FlxSprite(-200,-200).makeGraphic(FlxG.width * 2, FlxG.height * 2, FlxColor.BLACK);
+					black.scrollFactor.set();
+					black.alpha = 0;
+					add(black);
+					FlxTween.tween(black, {alpha: 1}, 2, {onComplete: function(twn:FlxTween) {
+						endSong();
+					}, startDelay: 5});
+				});
+			}, ease: FlxEase.linear});
+		});
+		FlxG.sound.play(Paths.sound('bf_transform'));
+	}
+
+	private function KillNotes() {
+		while(notes.length > 0) {
+			var daNote:Note = notes.members[0];
+			daNote.active = false;
+			daNote.visible = false;
+
+			FlxTween.cancelTweensOf(daNote);
+			daNote.kill();
+			notes.remove(daNote, true);
+			daNote.destroy();
+		}
+		unspawnNotes = [];
+		psychicNotes = [];
+	}
+
+	var transitioning = false;
+	function endSong():Void
+	{
+		timeBarBG.visible = false;
+		timeBar.visible = false;
+		timeTxt.visible = false;
+		canPause = false;
+		endingSong = true;
+		camZooming = false;
+		inCutscene = false;
+		updateTime = false;
+
+		deathCounter = 0;
+		seenCutscene = false;
+		KillNotes();
+
+		if(achievementObj != null) {
+			return;
+		} else {
+			var achieve:Int = checkForAchievement([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 14, 15, 16, 17]);
+			if(achieve > -1) {
+				startAchievement(achieve);
+				return;
+			}
+		}
+
+		if (SONG.validScore)
+		{
+			#if !switch
+			var percent:Float = ratingPercent;
+			if(Math.isNaN(percent)) percent = 0;
+			Highscore.saveScore(SONG.song, songScore, storyDifficulty, percent);
+			#end
+		}
+
+		if (isStoryMode)
+		{
+			campaignScore += songScore;
+			campaignMisses += songMisses;
+
+			storyPlaylist.remove(storyPlaylist[0]);
+
+			if (storyPlaylist.length <= 0)
+			{
+				FlxG.sound.playMusic(Paths.music('freakyMenu'));
+
+				transIn = FlxTransitionableState.defaultTransIn;
+				transOut = FlxTransitionableState.defaultTransOut;
+
+				FlxG.switchState(new StoryMenuState());
+
+				// if ()
+				StoryMenuState.weekUnlocked[Std.int(Math.min(storyWeek + 1, StoryMenuState.weekUnlocked.length - 1))] = true;
+
+				if (SONG.validScore)
+				{
+					Highscore.saveWeekScore(storyWeek, campaignScore, storyDifficulty);
+				}
+
+				FlxG.save.data.weekUnlocked = StoryMenuState.weekUnlocked;
+				FlxG.save.flush();
+				usedPractice = false;
+				changedDifficulty = false;
+			}
+			else
+			{
+				var difficulty:String = "";
+
+				if (storyDifficulty == 0)
+					difficulty = '-easy';
+
+				if (storyDifficulty == 2)
+					difficulty = '-hard';
+
+				trace('LOADING NEXT SONG');
+				trace(PlayState.storyPlaylist[0].toLowerCase() + difficulty);
+
+				var winterHorrorlandNext = (SONG.song.toLowerCase() == "eggnog");
+				if (winterHorrorlandNext)
+				{
+					var blackShit:FlxSprite = new FlxSprite(-FlxG.width * FlxG.camera.zoom,
+						-FlxG.height * FlxG.camera.zoom).makeGraphic(FlxG.width * 3, FlxG.height * 3, FlxColor.BLACK);
+					blackShit.scrollFactor.set();
+					add(blackShit);
+					camHUD.visible = false;
+
+					FlxG.sound.play(Paths.sound('Lights_Shut_off'));
+				}
+
+				FlxTransitionableState.skipNextTransIn = true;
+				FlxTransitionableState.skipNextTransOut = true;
+
+				prevCamFollow = camFollow;
+				prevCamFollowPos = camFollowPos;
+
+				PlayState.SONG = Song.loadFromJson(PlayState.storyPlaylist[0].toLowerCase() + difficulty, PlayState.storyPlaylist[0]);
+				FlxG.sound.music.stop();
+
+				if(winterHorrorlandNext) {
+					new FlxTimer().start(1.5, function(tmr:FlxTimer) {
+						LoadingState.loadAndSwitchState(new PlayState());
+					});
+				} else {
+					LoadingState.loadAndSwitchState(new PlayState());
+				}
+			}
+		}
+		else
+		{
+			trace('WENT BACK TO FREEPLAY??');
+			FlxG.switchState(new FreeplayState());
+			FlxG.sound.playMusic(Paths.music('freakyMenu'));
+			usedPractice = false;
+			changedDifficulty = false;
+		}
+	}
+	private function checkForAchievement(arrayIDs:Array<Int>):Int {
+		for (i in 0...arrayIDs.length) {
+			if(!Achievements.achievementsUnlocked[arrayIDs[i]][1]) {
+				switch(arrayIDs[i]) {
+					case 1 | 2 | 3 | 4 | 5 | 6 | 7:
+						if(isStoryMode && campaignMisses < 1 && songMisses < 1 && storyPlaylist.length <= 1 && storyWeek == arrayIDs[i] && !changedDifficulty && !usedPractice) {
+							Achievements.unlockAchievement(arrayIDs[i]);
+							return arrayIDs[i];
+						}
+					case 8:
+						if(songMisses < 1 && storyWeek == -99 && !changedDifficulty && !usedPractice) {
+							Achievements.unlockAchievement(arrayIDs[i]);
+							return arrayIDs[i];
+						}
+					case 9:
+						if(ratingPercent < 0.2 && !usedPractice) {
+							Achievements.unlockAchievement(arrayIDs[i]);
+							return arrayIDs[i];
+						}
+					case 10:
+						if(ratingPercent >= 1) {
+							Achievements.unlockAchievement(arrayIDs[i]);
+							return arrayIDs[i];
+						}
+					case 11:
+						if(Achievements.henchmenDeath >= 100) {
+							Achievements.unlockAchievement(arrayIDs[i]);
+							return arrayIDs[i];
+						}
+					case 12:
+						if(boyfriend.holdTimer >= 20 && !practiceMode) {
+							Achievements.unlockAchievement(arrayIDs[i]);
+							return arrayIDs[i];
+						}
+					case 13:
+						if(!boyfriendIdled && !usedPractice) {
+							Achievements.unlockAchievement(arrayIDs[i]);
+							return arrayIDs[i];
+						}
+					case 14:
+						if(!usedPractice) {
+							var howManyPresses:Int = 0;
+							for (j in 0...keysPressed.length) {
+								if(keysPressed[j]) howManyPresses++;
+							}
+
+							if(howManyPresses <= 2) {
+								Achievements.unlockAchievement(arrayIDs[i]);
+								return arrayIDs[i];
+							}
+						}
+					case 15:
+						if(ClientPrefs.framerate <= 60 && ClientPrefs.lowQuality && !ClientPrefs.globalAntialiasing) {
+							Achievements.unlockAchievement(arrayIDs[i]);
+							return arrayIDs[i];
+						}
+					case 16:
+						if(SONG.song.toLowerCase() == 'test' && !usedPractice) {
+							Achievements.unlockAchievement(arrayIDs[i]);
+							return arrayIDs[i];
+						}
+					case 17:
+						if(SONG.song.toLowerCase() == 'late-drive' && !usedPractice) {
+							Achievements.unlockAchievement(arrayIDs[i]);
+							return arrayIDs[i];
+						}
+				}
+			}
+		}
+		return -1;
+	}
+
+	var curLight:Int = 0;
+	var curLightEvent:Int = 0;
 }
