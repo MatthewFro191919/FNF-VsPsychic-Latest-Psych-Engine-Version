af48bb6eadb03ad3ebe16ad02421cf02af959cf3
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-07-27 09:55:14 -0400

    Update PsychicStage.hx

--- source/states/stages/PsychicStage.hx
+++ source/states/stages/PsychicStage.hx
@@ -32,35 +32,4 @@ class PsychicStage extends BaseStage
 	{
 				if(!ClientPrefs.data.lowQuality) animatedBGSprite.dance();
 	}
-
-	var doof:DialogueBox = null;
-	function initDoof()
-	{
-		var file:String = Paths.txt('$songName/${songName}Dialogue_${ClientPrefs.data.language}'); //Checks for vanilla/Senpai dialogue
-		#if MODS_ALLOWED
-		if (!FileSystem.exists(file))
-		#else
-		if (!OpenFlAssets.exists(file))
-		#end
-		{
-			file = Paths.txt('$songName/${songName}Dialogue');
-		}
-
-		#if MODS_ALLOWED
-		if (!FileSystem.exists(file))
-		#else
-		if (!OpenFlAssets.exists(file))
-		#end
-		{
-			startCountdown();
-			return;
-		}
-
-		doof = new DialogueBox(false, CoolUtil.coolTextFile(file));
-		doof.cameras = [camHUD];
-		doof.scrollFactor.set();
-		doof.finishThing = startCountdown;
-		doof.nextDialogueThing = PlayState.instance.startNextDialogue;
-		doof.skipDialogueThing = PlayState.instance.skipDialogue;
-	}
 }
