48790196eddc3ff31881dc8fca5c94e89ca3debc
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-07-27 08:27:54 -0400

    Create SpaceStage.hx

---
+++ source/states/stages/SpaceStage.hx
@@ -0,0 +1,33 @@
+package states.stages;
+
+import states.stages.objects.*;
+
+class SpaceStage extends BaseStage
+{
+	override function create()
+	{
+				var bg:FlxSprite = new FlxSprite(-600, -200).loadGraphic(Paths.image('space'));
+				bg.antialiasing = ClientPrefs.globalAntialiasing;
+				bg.scrollFactor.set(0.9, 0.9);
+				bg.active = false;
+				add(bg);
+
+				var asteroid:FlxSprite = new FlxSprite(-650, 600).loadGraphic(Paths.image('asteroid'));
+				asteroid.setGraphicSize(Std.int(asteroid.width * 1.1));
+				asteroid.updateHitbox();
+				asteroid.antialiasing = ClientPrefs.globalAntialiasing;
+				asteroid.scrollFactor.set(0.9, 0.9);
+				asteroid.active = false;
+				add(asteroid);
+
+				if(!ClientPrefs.lowQuality) {
+					var voyager:FlxSprite = new FlxSprite(-500, -300).loadGraphic(Paths.image('voyager'));
+					voyager.setGraphicSize(Std.int(voyager.width * 0.9));
+					voyager.updateHitbox();
+					voyager.antialiasing = ClientPrefs.globalAntialiasing;
+					voyager.scrollFactor.set(1.3, 1.3);
+					voyager.active = false;
+					add(voyager);
+				}
+	}
+}
